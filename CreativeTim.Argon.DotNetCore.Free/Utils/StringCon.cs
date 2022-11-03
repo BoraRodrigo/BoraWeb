75afb1b0ee92080bf1a2fd24665e78c97f2c7728
rodrigo.bora <rodrigo.bora@dbm.com.br>
2022-11-03 11:13:54 -0300

    Adicionar arquivos de projeto.

---
+++ CreativeTim.Argon.DotNetCore.Free/Utils/StringCon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JuntoSeguros.Utils
+{
+    public static class StringCon
+    {
+        public static IConfiguration Configuration { get; }
+        public static string ConString()
+        {
+            var ambiente = "P"; //PRODUCAO
+
+            string ConString = "";
+            if (ambiente == "P")
+            {
+                ConString = $"Server=localhost;Database=DEV_BORA_WEB;User Id=user_dev_bora_web; Password=Bb@222abcdegrb36";
+            }
+            else if (ambiente == "D")
+            {
+               
+            }
+            else if (ambiente == "H")
+            {
+               
+            }
+            return ConString;
+        }
+    }
+}
